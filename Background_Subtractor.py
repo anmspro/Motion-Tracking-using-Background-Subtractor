8ba3cee2ddfd47d023858b18595d277462ecfb51
Abu Noman Md. Sakib <33668152+anmspro@users.noreply.github.com>
2020-05-04 00:11:48 +0600

    Update

--- Background_Subtractor.py
+++ Background_Subtractor.py
@@ -1,19 +1,38 @@
+
 import cv2
 import numpy as np
 
 cap = cv2.VideoCapture(0)
 out = cv2.createBackgroundSubtractorMOG2()
 
-while True:
-    ret, frame = cap.read()
-    outmask = out.apply(frame)
-    
-    cv2.imshow('original', frame)
-    cv2.imshow('out', outmask)
-    
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
-        break
+fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+output = cv2.VideoWriter('output.avi', fourcc, 20.0, (640,480))
 
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    
+    if ret==True:
+        frame = cv2.flip(frame,180)
+        
+        #output.write(frame)
+           
+        outmask = out.apply(frame)
+        #print(frame.shape[0], frame.shape[1])
+        #print(outmask.shape[0], outmask.shape[1])
+        #print("\n")
+        
+        output.write(outmask)
+        
+        cv2.imshow('original', frame)
+        cv2.imshow('Motion Tracker', outmask)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    else:
+        break
+    
+
+output.release()
 cap.release()
 cv2.destroyAllWindows()
